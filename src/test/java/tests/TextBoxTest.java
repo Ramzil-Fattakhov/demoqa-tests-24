60e163b7aeab69608c855951a80567919e0de8b5
Ramzil Fattakhov <ramzilf@gmail.com>
2024-05-07 17:46:33 +0300

    added TextBoxTests

---
+++ src/test/java/tests/TextBoxTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000;
+    }
+
+        @Test
+        void fillFormTest() {
+            open("/text-box");
+            $("#userName").setValue("Alex");
+            $("#userEmail").setValue("Alex@gmail.com");
+            $("#currentAddress").setValue("Street 1");
+            $("#permanentAddress").setValue("Street 2");
+            $("#submit").click();
+
+            $("#output #name").shouldHave(text("Alex"));
+            $("#output #email").shouldHave(text("Alex@gmail.com"));
+            $("#output #currentAddress").shouldHave(text("Street 1"));
+            $("#output #permanentAddress").shouldHave(text("Street 2"));
+        }
+    }
