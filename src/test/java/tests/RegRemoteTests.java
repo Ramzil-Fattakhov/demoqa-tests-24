c65af5f62ff410c0e06e8f369a92b2d4a1575cfd
Ramzil Fattakhov <ramzilf@gmail.com>
2025-02-10 19:56:05 +0300

    edited build.gradle and added RegRemoteTests.java

--- src/test/java/tests/RegRemoteTests.java
+++ src/test/java/tests/RegRemoteTests.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class RegistrationRemoteTests {
+public class RegRemoteTests {
 
     @BeforeAll
     static void beforeAll() {
@@ -23,6 +23,7 @@ public class RegistrationRemoteTests {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
+    @Tag("demoqa")
     @Test
     void successfulRegistrationTest() {
         step("Open from", () -> {
