f52bd4f54f60d8eab9451df8e5de7ee9e5f3a8c7
Ramzil Fattakhov <ramzilf@gmail.com>
2025-02-24 21:25:53 +0300

    added property_test

--- src/test/java/properties/tests/SystemPropertiesTests.java
+++ src/test/java/properties/tests/SystemPropertiesTests.java
@@ -3,6 +3,8 @@ package properties.tests;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
 public class SystemPropertiesTests {
 
     @Test
@@ -36,5 +38,13 @@ public class SystemPropertiesTests {
     void systemProperties4Test() {
         String browser = System.getProperty("browser", "mozilla");
         System.out.println(browser);
+    }
+
+    @Test
+    @Tag("hello")
+    void systemProperties5Test() {
+        String name = System.getProperty("name", "default student");
+        String message = format("Hello, %s!", name);
+        System.out.println(message);
     }
 }
